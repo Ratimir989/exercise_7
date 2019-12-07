5bf9a5be32a42ba1bc9f6c9efc9514f67af91335
Ratimir <ra0953029364@gmail.com>
2019-12-07 16:50:10 +0200

    Добавьте файлы проекта.

---
+++ exercise_7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace exercise_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            double num1 = double.Parse(Console.ReadLine());
+            double z = num1 % 2;
+            if (z != 0)
+            {
+                Console.WriteLine("Число не являеться простым");
+            }
+            else
+            {
+                Console.WriteLine("Число простое");
+            }
+        }
+    }
+}
